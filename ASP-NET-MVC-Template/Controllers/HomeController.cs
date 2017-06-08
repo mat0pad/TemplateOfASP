de33a13dc5efdc5b4973972a871f9358d7f51b15
Mathias Lykke Pedersen <none>
2017-06-08 13:33:17 +0200

    init

---
+++ ASP-NET-MVC-Template/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace ASPNETMVCTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var mvcName = typeof(Controller).Assembly.GetName();
+            var isMono = Type.GetType("Mono.Runtime") != null;
+
+            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+            // Check om async form client
+            /*if (Request.IsAjaxRequest())
+				return PartialView("Confirmation");
+			*/
+
+            return View();
+        }
+
+        // Eksempel på func der kaldes fra klient via AJAX dette eksempel på !POST!
+        public string Login(string password, string email)
+        {
+            Console.WriteLine("Server received call!");
+            Console.WriteLine("pass: " + password + ", mail: " + email);
+            if (password == "test" && email == "test@test.com")
+                return "{ \"object\" : \"Succes - " + password + ", " + email + "'\" }";
+            else
+                return "Failed login with credentials - " + password + ", " + email;
+        }
+
+
+        // Eksempel på func der kaldes fra klient via AJAX dette eksempel på !GEt!
+        public string Navn(int nr)
+        {
+            var Stringarray = new string[] { "anton", "abe" };
+
+            if (nr >= 0 && nr <= 1)
+                return Stringarray[nr];
+            else
+                return "Error!!!!!";
+        }
+    }
+}
---
+++ ASP-NET-MVC-Template/Scripts/index.js
@@ -0,0 +1,114 @@
+﻿
+window.onload = function() {
+
+    // Do something when view loads
+    //alert("Siden er loadet!");
+    console.log("Siden er loadet!");
+};
+
+
+
+// Clicked login
+$("#dumKnap").click(function(e) {
+ 
+    // uden jquery
+    //var input = document.getElementById('inputFelt').value;
+
+    // med jquery
+    var input = $("#inputFelt").val();
+
+    alert("Du har skrevet: " + input);
+});
+
+//On form submit
+$('#box').on('submit', function(e) {
+
+    e.preventDefault(); // undgå page refresh hvis type = submit ved form
+
+    console.log("Submit pressed");
+
+    sendKnap(e);
+});
+
+
+// Brug af simplet brug AJAX POST
+function sendKnap(e){
+
+    //collects data from all the form inputs
+    var ajaxData = new FormData($('#box').get(0));
+
+    ajaxData.append('password', $('#passwordInput').val() );
+    ajaxData.append('email', $('#mailInput').val() );
+
+    // server comunication here 
+    $.ajax({
+        url: "Home/Login",
+        type: $('#box').attr('method'), // "GET" or "POST"
+        data: ajaxData,                 //  data: { "email=" : email, "password=" : password},
+        dataType: 'json', 
+        cache: false,
+        contentType: false,
+        processData: false,
+        complete: function() {
+
+            console.log("Done making the call");
+        },
+        success: function(response) {
+
+            responeHandler(response);
+        },
+        error: function(data, status, error) {
+
+            console.log(data.responseText);
+
+            alert(data.responseText);
+        }
+    });
+}
+
+//Handler response here!
+function responeHandler(result) {
+    // code that depends on `result`
+    console.log("Response is: ");
+    console.log(result);
+
+    alert(result);
+}
+
+// Clicked hentNavn
+$("#hentNavn").click(function(e) {
+ 
+    hentNavn(e);
+});
+
+// Brug af simplet brug AJAX GET
+function hentNavn(e){
+
+    //collects data from all the form inputs
+    var data = $("#inputFelt").val();
+
+    // server comunication here 
+    $.ajax({
+        url: "Home/Navn",
+        type: 'GET', // "GET" or "POST"
+        data: "nr=" + data,                 //  data: { "email=" : email, "password=" : password},
+        dataType: 'text/plain', 
+        cache: false,
+        contentType: false,
+        processData: false,
+        complete: function() {
+
+            console.log("Done making the call");
+        },
+        success: function(response) {
+
+            responeHandler(response);
+        },
+        error: function(data, status, error) {
+
+            console.log(data.responseText);
+
+            alert(data.responseText);
+        }
+    });
+}
